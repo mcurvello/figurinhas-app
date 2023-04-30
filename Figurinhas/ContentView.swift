244da3070a23af15cb8b9c53fe4e94d145847a4b
Marcio Curvello <marciocurvello@gmail.com>
2023-04-30 02:09:30 -0300

    figurinhas app

--- Figurinhas/ContentView.swift
+++ Figurinhas/ContentView.swift
@@ -9,13 +9,51 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+       
+        VStack(alignment: .leading) {
+            
+            Text("Mingle")
+                        .font(.title)
+                        .fontWeight(.black)
+                        .foregroundColor(Color.gray)
+                        .bold()
+                        .textCase(/*@START_MENU_TOKEN@*/.lowercase/*@END_MENU_TOKEN@*/)
+            HStack {
+                Text("Frase: ")
+                    .bold()
+                Text("Problema em dobro")
+            }
+            
+            HStack {
+                Image("figura_01")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100)
+                VStack {
+                    HStack{
+                        Image(systemName: "lifepreserver")
+                            .foregroundColor(Color.green)
+                            .font(.system(size: 30))
+                        Text("Vidas:")
+                        Spacer()
+                        Text("2")
+                    }
+                    HStack{
+                        Image(systemName: "bolt")
+                            .foregroundColor(Color.yellow)
+                            .font(.system(size: 30))
+                        Text("Potência:")
+                        Spacer()
+                        Text("60%")
+                    }
+                }
+                
+                
+            }
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            Spacer()
         }
-        .padding()
+        
     }
 }
 
