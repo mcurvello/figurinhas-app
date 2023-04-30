94edacb74cd43f558d21beb86292f6622a4cf481
Marcio Curvello <marciocurvello@take.net>
2023-04-30 01:06:22 -0300

    Initial Commit

---
+++ Figurinhas/FigurinhasApp.swift
@@ -0,0 +1,17 @@
+//
+//  FigurinhasApp.swift
+//  Figurinhas
+//
+//  Created by Marcio Curvello on 30/04/23.
+//
+
+import SwiftUI
+
+@main
+struct FigurinhasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
