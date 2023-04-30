6bf7f015eb907c65a0b1e2c9df73b0a8fa4debc7
Marcio Curvello <marciocurvello@gmail.com>
2023-04-30 11:34:56 -0300

    layout screen completed

--- Figurinhas/ContentView.swift
+++ Figurinhas/ContentView.swift
@@ -14,43 +14,51 @@ struct ContentView: View {
             
             Text("Mingle")
                         .font(.title)
-                        .fontWeight(.black)
-                        .foregroundColor(Color.gray)
                         .bold()
-                        .textCase(/*@START_MENU_TOKEN@*/.lowercase/*@END_MENU_TOKEN@*/)
+                        .padding()
             HStack {
                 Text("Frase: ")
                     .bold()
-                Text("Problema em dobro")
+                Text("\"Problema em dobro\"")
             }
+            .padding(.leading)
             
-            HStack {
+            HStack() {
+                Spacer()
                 Image("figura_01")
                     .resizable()
                     .scaledToFit()
                     .frame(width: 100)
-                VStack {
+                VStack(alignment: .leading) {
                     HStack{
                         Image(systemName: "lifepreserver")
                             .foregroundColor(Color.green)
                             .font(.system(size: 30))
+                            .frame(width: 30.0)
                         Text("Vidas:")
-                        Spacer()
                         Text("2")
+                            .padding(.trailing)
                     }
                     HStack{
                         Image(systemName: "bolt")
                             .foregroundColor(Color.yellow)
                             .font(.system(size: 30))
+                            .frame(width: 30.0)
                         Text("Potência:")
-                        Spacer()
                         Text("60%")
                     }
                 }
-                
+                Spacer()
                 
             }
-            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            .padding(30)
+            HStack {
+                Spacer()
+                Text("O Mingle se destaca por fazer o dobro do trabalho na metade do tempo, com extrema precisão. Essas habilidades são úteis para ela em sua função de Analista de Dados Sênior para uma empresa internacional de computação em nuvem. Ela também tem uma propensão para dança de salão, dança de linha e praticamente qualquer tipo de atividade que a deixe dançar ao som da música.")
+                    .font(.body)
+                    .foregroundColor(Color.gray)
+                Spacer()
+            }
             Spacer()
         }
         
